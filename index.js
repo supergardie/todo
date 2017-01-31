1641ef8b2320b8e5d8422443fb0773d340905d64
Samantha Ferreira <samanthaf@i3inc.ca>
2017-01-31 14:50:53 -0500

    Basic functionality. Can add and delete.

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 "use strict";
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var loki = require('lokijs');
+var db = new loki('todo.db');
+var index = path.join(__dirname + "/index.html");
 var app = express();
+var todos = db.addCollection('todos');
+var listCount = 0;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
-    res.send('Hello Earth!');
+    res.sendFile(index);
+});
+app.post("/todo", function (req, res) {
+    var item = req.body.todoItem;
+    addItems(item);
+    var listItems = retrieveItems();
+    res.send("Added " + item + " to list. Full list: " + listItems + ". | <a href=\"/\">Go back?</a>");
+});
+app.post("/del", function (req, res) {
+    var numDel = req.body.itemNum;
+    numDel = parseInt(numDel);
+    var delItem = todos.findOne({ num: numDel });
+    var delItem = delItem.listItem;
+    deleteItems(numDel);
+    var listItems = retrieveItems();
+    res.send("Removed " + delItem + " from the list. Full list: " + listItems + ". | <a href=\"/\">Go back?</a>");
 });
 app.listen(3000, function () {
     console.log("Listening on port 3000");
 });
+var retrieveItems = function () {
+    var printItem;
+    var listItems = [];
+    for (var ii = 0; ii < listCount; ii++) {
+        printItem = todos.findOne({ num: ii });
+        if (printItem) {
+            listItems.push(printItem.listItem);
+        }
+    }
+    return listItems;
+};
+var deleteItems = function (item) {
+    todos.remove(todos.findOne({ num: item }));
+};
+var addItems = function (item) {
+    todos.insert({ num: listCount++, listItem: item });
+};
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,12 +1,77 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as path from "path";
+const loki = require('lokijs');
 
-let app = express();
+const db = new loki('todo.db');
+const index = path.join(__dirname + "/index.html");
+const app = express();
+
+var todos = db.addCollection('todos');
+var listCount = 0;
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get("/", function(req, res) {
-    res.send('Hello Earth!');
+    res.sendFile(index);
 });
+
+
+
+app.post("/todo", function(req, res) {
+    let item = req.body.todoItem;
+    addItems(item);
+    
+    var listItems = retrieveItems();
+
+    res.send(`Added ${item} to list. Full list: ${listItems}. | <a href="/">Go back?</a>`);
+});
+
+
+
+app.post("/del", function(req, res) {
+    let numDel = req.body.itemNum;
+    numDel = parseInt(numDel);
+
+    var delItem = todos.findOne({num: numDel});
+    var delItem = delItem.listItem;
+    deleteItems(numDel);
+
+    var listItems = retrieveItems();
+    res.send(`Removed ${delItem} from the list. Full list: ${listItems}. | <a href="/">Go back?</a>`)
+});
+
+
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 });
+
+
+
+let retrieveItems = function() {
+    let printItem;
+    let listItems = [];
+    for(var ii = 0; ii < listCount; ii++) {
+        printItem = todos.findOne({num: ii});
+        if(printItem) {
+            listItems.push(printItem.listItem);
+        }
+    }
+
+    return listItems;
+}
+
+
+let deleteItems = function(item) {
+    todos.remove(todos.findOne({num: item}));
+}
+
+
+let addItems = function(item) {
+    todos.insert({num: listCount++, listItem: item});    
+}
